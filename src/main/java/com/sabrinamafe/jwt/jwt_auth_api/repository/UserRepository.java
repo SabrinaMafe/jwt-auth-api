f4957b0f6298913fc23a7da0fdef3379776046c4
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2025-07-10 16:36:16 -0300

    add User model and repository for user management

---
+++ src/main/java/com/sabrinamafe/jwt/jwt_auth_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sabrinamafe.jwt.jwt_auth_api.repository;
+
+import com.sabrinamafe.jwt.jwt_auth_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
