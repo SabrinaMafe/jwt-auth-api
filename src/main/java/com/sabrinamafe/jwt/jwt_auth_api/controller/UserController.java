eb2d08a10806c557fe899ac79bf2173ad717acdf
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2025-07-10 16:47:11 -0300

    add UserController for retrieving all users

---
+++ src/main/java/com/sabrinamafe/jwt/jwt_auth_api/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.sabrinamafe.jwt.jwt_auth_api.controller;
+
+import com.sabrinamafe.jwt.jwt_auth_api.model.User;
+import com.sabrinamafe.jwt.jwt_auth_api.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+}
